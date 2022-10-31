31e77063b36a356fbc909a1be1ac79af534203a6
Marcus20119 <hoanglongnguyenhuu20119@gmail.com>
2022-10-31 11:59:26 +0700

    frame1 done

--- src/components/Frame1/index.jsx
+++ src/components/Frame1/index.jsx
@@ -1,12 +1,14 @@
 import styles from './Frame1.module.scss';
 import MainContent from './MainContent';
 import Navbar from './Navbar';
+import SearchBar from './SearchBar';
 
 const Frame1 = () => {
   return (
     <div className={styles.frame1}>
       <Navbar />
       <MainContent />
+      <SearchBar />
     </div>
   );
 };
