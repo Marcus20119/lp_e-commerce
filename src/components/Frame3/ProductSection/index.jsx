de27c2ad0def2f3ce73dce004453973048dbd899
duchuy <dhuy221001@gmail.com>
2022-10-31 17:02:33 +0700

    release frame3

--- src/components/Frame3/ProductSection/index.jsx
+++ src/components/Frame3/ProductSection/index.jsx
@@ -1,10 +1,31 @@
 import styles from './ProductSection.module.scss'
 import { Fragment } from "react";
+import PrimaryButton from '~/components/common/PrimaryButton';
 
 function ProductSection(){
     return(
         <Fragment>
-            
+            <div className={styles.sectionHeader}>
+                <h1>Our best seller product</h1>
+                <PrimaryButton
+                    widthType="fit"
+                    style={{
+                      fontFamily: "'Roboto', sans-serif",
+                      paddingRight: '30px',
+                      paddingLeft: '30px',
+                    }}
+                >
+                    <span>See all Product</span>
+                    <svg 
+                        width="27" 
+                        height="14" 
+                        viewBox="0 0 27 14" 
+                        fill="none" 
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path d="M1 7H26M26 7L20.2308 1M26 7L20.2308 13" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                    </svg>
+                </PrimaryButton>
+            </div>
         </Fragment>
     )
 
--- src/components/Frame3/ProductThumbnail/index.jsx
+++ src/components/Frame3/ProductThumbnail/index.jsx
@@ -1,11 +1,52 @@
 import styles from './ProductThumnail.module.scss'
 import { Fragment } from 'react'
+import PrimaryButton from '~/components/common/PrimaryButton'
 
 function ProductThumnail(){
+    const products =[
+        {
+            productImg:'T-sirt.png',
+            productName: 'T-sirt',
+            productPrice:'40',
+            discount: '100$',
+        },
+        {
+            productImg:'Coat.png',
+            productName: 'Coat',
+            productPrice:'200',
+            discount: '50$',
+        },
+        {
+            productImg:'Sweater.png',
+            productName: 'Sweater',
+            productPrice:'300',
+            discount: '100$',
+        },
+        {
+            productImg:'Vest.png',
+            productName: 'Vest',
+            productPrice:'500',
+            discount: '200$',
+        },
+    ]
     return(
-        <Fragment>
-            
-        </Fragment>
+        <div>
+            <div className={styles.thumbnailContainer}>
+                {products.map((product,index) => {
+                    return(
+                        <div key={index} className={styles.productCard}>
+                            <img src={product.productImg} alt={product.productName} />
+                            <div className={styles.productInfo}>
+                                <h3>{product.productName}</h3>
+                                <h3>{product.productPrice}<span>$</span></h3>
+                                <p>{product.discount}</p>
+                                <button className={styles.buttonBuy}>Buy</button>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
     )
 }
 export default ProductThumnail
