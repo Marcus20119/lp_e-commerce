6ac57fa63cb6d48f129755a5eddce2996538e2d5
duchuy <dhuy221001@gmail.com>
2022-10-31 13:55:28 +0700

    release frame2

--- src/components/Frame2/index.jsx
+++ src/components/Frame2/index.jsx
@@ -1,5 +1,11 @@
+import styles from './Frame2.module.scss';
+import MainContent from './MainContent';
 const Frame2 = () => {
-  return <div></div>;
+  return (
+    <div className={styles.frame2}>
+        <MainContent />
+    </div>
+    );
 };
 
 export default Frame2;
