d3375f13e2485a4dbda4067ff23d891538e198ef
duchuy <dhuy221001@gmail.com>
2022-10-31 15:18:25 +0700

    update frame3-snapshot

---
+++ src/components/Frame3/ProductSection/index.jsx
@@ -0,0 +1,12 @@
+import styles from './ProductSection.module.scss'
+import { Fragment } from "react";
+
+function ProductSection(){
+    return(
+        <Fragment>
+            
+        </Fragment>
+    )
+
+}
+export default ProductSection;
---
+++ src/components/Frame3/ProductThumbnail/index.jsx
@@ -0,0 +1,11 @@
+import styles from './ProductThumnail.module.scss'
+import { Fragment } from 'react'
+
+function ProductThumnail(){
+    return(
+        <Fragment>
+            
+        </Fragment>
+    )
+}
+export default ProductThumnail
