e90f31412f2aeac748b4382a44c1afbb42fc24cc
Marcus20119 <hoanglongnguyenhuu20119@gmail.com>
2022-11-02 11:37:01 +0700

    update the rest frame components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
+import { Fragment } from 'react';
 import Frame1 from '~/components/Frame1';
 import Frame2 from '~/components/Frame2';
 import Frame3 from '~/components/Frame3';
+import Frame4 from '~/components/Frame4';
+import Frame5 from '~/components/Frame5';
+import Frame6 from '~/components/Frame6';
+import Frame7 from '~/components/Frame7';
+import Frame8 from '~/components/Frame8';
+import Frame9 from '~/components/Frame9';
 import styles from './App.module.scss';
 
 function App() {
   return (
-    <div className={styles.App}>
-      <Frame1 />
-      <Frame2 />
-      <Frame3 />
-    </div>
+    <Fragment>
+      <div className={styles.App}>
+        <Frame1 />
+        <Frame2 />
+        <Frame3 />
+        <Frame4 />
+        <Frame5 />
+        <Frame6 />
+        <Frame7 />
+        <Frame8 />
+      </div>
+      <Frame9 />
+    </Fragment>
   );
 }
 
---
+++ src/components/Frame8/index.jsx
@@ -0,0 +1,5 @@
+const Frame8 = () => {
+  return <div></div>;
+};
+
+export default Frame8;
