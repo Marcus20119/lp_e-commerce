92cfb1fcd9d16621777a116445d93ea11d75c464
duchuy <dhuy221001@gmail.com>
2022-11-03 13:08:03 +0700

    update fram8

---
+++ src/components/Frame8/Navbar/index.jsx
@@ -0,0 +1,30 @@
+import styles from './Navbar.module.scss'
+import { useState } from 'react';
+const navItems= ['PAYMENTS', 'REFUND','PRICING', 'SHIPPING', 'OTHER']
+const Navbar = () => {
+    const [active,setActive]=useState(2)
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>Have Any Question ?</h1>
+            <div className={styles.navbar}>
+                {navItems.map((item,index)=>{
+                    return (
+                        // eslint-disable-next-line jsx-a11y/anchor-is-valid
+                        <a 
+                            key={index}
+                            href="#"
+                            style={active===index?{color:'var(--primary-color)'}:{}}
+                            onClick={(e)=>{
+                                setActive(index);
+                                e.preventDefault();
+                            }}
+                        >
+                            {item}
+                        </a>
+                    )
+                })}
+            </div>
+        </div>
+    )
+};
+export default Navbar;
--- src/components/Frame8/index.jsx
+++ src/components/Frame8/index.jsx
@@ -1,5 +1,13 @@
+import styles from './Frame8.module.scss'
+import Navbar from './Navbar';
+import Information from './Information';
 const Frame8 = () => {
-  return <div></div>;
+  return (
+    <div className={styles.Frame8}>
+      <Navbar />
+      <Information />
+    </div>
+  )
 };
 
 export default Frame8;
